b1fd51212a0c450a6c95475b5b8dd13fe7b753ec
Dev Sharma <devsharma1683@gmail.com>
2025-07-14 16:27:47 +0530

    a response template for make consistence response handling in codebase

---
+++ src/utils/apiResponse.js
@@ -0,0 +1,10 @@
+// ? this file is for making the 'response' consistence throughout the codebase 
+
+class ApiResponse {
+    constructor(statusCode, data, message= "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
