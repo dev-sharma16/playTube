f384c731f4bc5b08874e5a1c8a976c134c356aea
Dev Sharma <devsharma1683@gmail.com>
2025-07-14 16:17:37 +0530

    add code for starting the server

--- src/index.js
+++ src/index.js
@@ -31,9 +31,24 @@ const app = express();
 // require('dotenv').config({path: './env'}) //* You use ths don't need do do config, but not for consistency in code.
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    // ? its a good practise to listen errors first
+    // app.on("error", (error) => {
+            // console.log("ERROR: ",error);
+            // throw error;
+    // })
+
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is Started at Port: ,${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("MONGO DB connection failed.! ", error);
+})
