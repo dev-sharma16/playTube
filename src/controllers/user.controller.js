09ecc4c084c4c367e58607f8c23f18d0d32bfdab
Dev Sharma <devsharma1683@gmail.com>
2025-07-17 23:08:28 +0530

    added login and logout function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,25 @@ import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/apiResponse.js"
 
+const generateAccessAndRefreshToken = async(req,res) => {
+    try {
+        const user =  await User.findById(userId);
+
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false}) //* while because we are saving only one field and it don't have password with them, so we need to add 'validateBeforeSave' and due to that mongoDB don't need validation
+
+        // TODO we did'nt add accessToken in DB
+
+        return { accessToken, refreshToken }
+
+    } catch (error) {
+        throw new ApiErrors(500, "Something went Wrong while generating its refresh or access token")
+    }
+}
+
 const registerUser = asyncHandler ( async (req, res) => {
     //? Steps to register user
     //* get uessr details from body
@@ -82,4 +101,86 @@ const registerUser = asyncHandler ( async (req, res) => {
     )
 })
 
-export { registerUser }
+const loginUser = asyncHandler ( async (req,res) => {
+    // ? Steps to login a user :-
+    // * take the data from body
+    // * check weither you received the username or email
+    // * find the user nased in the input
+    // * then check the user password you find one in db
+    // * if password is correct then generate 'refresh' and 'access' token and send to user both
+    // * then send those token in cokkies
+
+    const {email, username, password} = req.body;
+
+    if(!username || !email) throw new ApiErrors(400, "Username or email is Required");
+
+    const user = await User.findOne({
+        $or: [{username}, {email}]
+    });
+
+    if(!user) throw new ApiErrors(404,"User not exists");
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if(!isPasswordValid) throw new ApiErrors(401, "Password is not Correct");
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id);
+
+    const loggedInUser = await User.findById(user._id).select("-password", "-refreshToken") //* removing the passwrod and refreshToken from 'user' object 
+
+    // ? When we pass 'true' in the 'httOnly' and 'secure' then the 'Cookies' become unEditable from the frontend
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200, 
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User Logged In Successfully"
+        )
+    )
+
+})
+
+const logoutUser = asyncHandler ( async (req,res) => {
+    //req.user //? we can access this user details object because of the auth middleware  
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            //TODO research about mongoDb operators
+            $set: {
+                refreshToken: undefined
+            } 
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(
+        new ApiResponse(
+            200, 
+            {},
+            "User Logged Out Successfully"
+        )
+    )
+})
+
+export { registerUser, loginUser, logoutUser }
