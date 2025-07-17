4964cf85bfba3ce0fa674b3127316de542993f39
Dev Sharma <devsharma1683@gmail.com>
2025-07-17 23:12:21 +0530

    added a route for login and logout function the and in logout route i added the auth middleware (verifyJwt)

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
@@ -16,6 +17,17 @@ router.route("/register").post(
         }
     ]),
     registerUser
+)
+
+router.route("/login").post(
+    loginUser
+)
+
+// * Secured Routes
+router.route("/logout").post(
+    //? if you want to use middleware before a certain function just add before it, and that's why we add 'next()' in middleware so it know then after middleware execution the next function executes which is after that.
+    verifyJwt, 
+    logoutUser
 )
 
 export default router
